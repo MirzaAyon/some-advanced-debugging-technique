77776c060a04ad73f7e4a640ade4b187ba037210
MirzaAyon <ayonmirza98@gmail.com>
2022-07-31 13:33:03 +0600

    done with fancy slide debug

---
+++ fancy slide/bug-report.js
@@ -0,0 +1,38 @@
+//kono kichu likhe search dile
+//console e 
+TypeError: Cannot read properties of undefined (reading 'forEach')
+    at showImages (app.js:22:10)
+    at app.js:34:19
+    line 35 e problem
+
+    //22.10 code tae click she amk shei code tae niye jabe
+    //bp dibo
+    //backward approach - bp jekhane dewa ache tar age age jabe bug ta dhorar jnno
+    //abr some times kono ekta framework er vitore code dhuke jete pare
+
+    //ebar forward approach
+    //22 e bp dewa obosthae
+    //search e right click
+    //erpr inspact
+    //ekta btn e niye jabe
+    //elements er modhe niye jabe
+    //daan pashe click kore dekhbo kono event handler ache kina
+    //hmm 112 te ache
+    //shetae click kore chole jabe
+    //113 no line e bp dilam
+    //erpr search e click korlam
+    //querySelector('.main) 
+    //etar mane main class er prothom take select krsi
+
+
+    //onno ekta console e 
+    //const nums = [4,3,2];
+// undefined
+// nums.length
+// 3
+// nums.length = 0
+// 0
+// nums
+// []
+//eta try krbo
+//forward and backward approach apply kore debug korar try krbo
---
+++ fancy slide/bug-report2-withconsole.js
@@ -0,0 +1,5 @@
+//control f marle oi file er jinish dekhabe
+//control shift f marle sob file e kothae ki ache ta baam pashe dekhabe
+//duration - > doration
+//hitS -> hits
+//conceptual and main course er shob stretagy apply krbo
